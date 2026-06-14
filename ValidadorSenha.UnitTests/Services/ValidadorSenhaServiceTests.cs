2be6d5c019016eacd53b8d7c5b4dd5547deb50ac
Nicolas Reis <nicolasreissanto@gmail.com>
2026-06-14 11:04:27 -0300

    add testes unitários para validação de senha

---
+++ ValidadorSenha.UnitTests/Services/ValidadorSenhaServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValidadorSenha.Api.Application.Services.Impl;
+using Xunit;
+
+namespace ValidadorSenha.UnitTests.Services
+{
+    public class ValidadorSenhaServiceTests
+    {
+        private readonly ValidadorSenhaService _service;
+
+        public ValidadorSenhaServiceTests()
+        {
+            _service = new ValidadorSenhaService();
+        }
+
+        [Theory]
+        [InlineData("", false)]
+        [InlineData("aa", false)]
+        [InlineData("ab", false)]
+        [InlineData("AAAbbbCc", false)]
+        [InlineData("AbTp9!foo", false)]
+        [InlineData("AbTp9!foA", false)]
+        [InlineData("AbTp9 fok", false)]
+        [InlineData("AbTp9!fok", true)]
+        public void SenhaEhValida_DeveRetornarResultadoConformeRegrasDeValidacao(
+            string senha,
+            bool resultadoEsperado)
+        {
+            var resultado = _service.SenhaEhValida(senha);
+
+            Assert.Equal(resultadoEsperado, resultado);
+        }
+    }
+}
